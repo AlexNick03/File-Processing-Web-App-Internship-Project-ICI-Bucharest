4e688714a4a48d6c2370099237ec2d6788ee805a
AlexNick03 <73990514+AlexNick03@users.noreply.github.com>
2025-08-13 21:54:38 +0300

    Schimbare logo , completare  main page + fetch pe generare de pagini
    Am decis ca arhitectura  proiectului va folosi fetch epntru a randa cntinutul din mainul indexului , si vom foloci s3 pentru a stoca frontednul in aws

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,4 +1,18 @@
 let mainEl = document.querySelector('.main-section')
+let wordToPdfEl = document.querySelector('.word-navigation')
+let  homEl = document.querySelector('.home-navigation')
+
+homEl.addEventListener('click', () => {
+    fetch('./pages/home.html')
+    .then(response => response.text()) // convert to plain text and passes to next  
+    .then(html => mainEl.innerHTML = html) // insert into main section
+})
+wordToPdfEl.addEventListener('click', () => {
+    fetch('./pages/word-pdf.html')
+    .then(response => response.text()) // convert to plain text and passes to next  
+    .then(html => mainEl.innerHTML = html) // insert into main section
+})
+
 fetch('./pages/home.html')
     .then(response => response.text()) // convert to plain text and passes to next  
     .then(html => mainEl.innerHTML = html) // insert into main section
