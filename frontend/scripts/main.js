0bc66a1f648779719a2834769d3f6c4093bce4fb
AlexNick03 <73990514+AlexNick03@users.noreply.github.com>
2025-08-14 17:57:50 +0300

    Restructurare proiect  + frontend pentru functionalitati

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,19 +1,17 @@
 let mainEl = document.querySelector('.main-section')
-let wordToPdfEl = document.querySelector('.word-navigation')
+let wordToPdfEl = document.querySelector('.file-convertor')
+let excelEl = document.querySelector('.excel-navigation')
 let  homEl = document.querySelector('.home-navigation')
 
-homEl.addEventListener('click', () => {
-    fetch('./pages/home.html')
-    .then(response => response.text()) // convert to plain text and passes to next  
-    .then(html => mainEl.innerHTML = html) // insert into main section
-})
 wordToPdfEl.addEventListener('click', () => {
-    fetch('./pages/word-pdf.html')
+    fetch('./pages/file-convertor.html')
     .then(response => response.text()) // convert to plain text and passes to next  
     .then(html => mainEl.innerHTML = html) // insert into main section
 })
 
-fetch('./pages/home.html')
+
+ excelEl.addEventListener('click', () => {
+    fetch('./pages/excel-cleanup.html')
     .then(response => response.text()) // convert to plain text and passes to next  
     .then(html => mainEl.innerHTML = html) // insert into main section
- 
+})
